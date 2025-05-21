9ebba05dc91c356b79937eb1a199e7ac15b2482c
lidiya-fiker <lidiyafikerr@gmail.com>
2025-05-21 14:48:52 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,22 @@ import Register from "./pages/auth/RegisterAs";
 import Login from "./pages/auth/Login";
 import CounselorRegister from "./pages/counselor/counselor-complete-profile";
 import EmailVerification from "./pages/auth/EmailVerification";
-// import ClientCompleteProfile from "./pages/client/ClientProfile";
-// import ClientProfileNext from "./pages/client/Client-complete-profile";
-// import ClientDashboard from "./pages/client/ClientDashboard";
-// import CounselorPosts from "./pages/client/CounselorPosts";
+import ClientCompleteProfile from "./pages/client/ClientProfile";
+import ClientProfileNext from "./pages/client/Client-complete-profile";
+import ClientDashboard from "./pages/client/ClientDashboard";
+import CounselorPosts from "./pages/client/CounselorPosts";
 import CounselorDashboard from "./pages/counselor/CounselorDashboard";
-// import CounselorProfile from "./pages/counselor/CounelorDetail";
-// import CounselorAvailability from "./pages/counselor/CounselorAvailability";
-// import CounselorSchedule from "./pages/counselor/CounselorSchedule";
-// import CounselorFeedback from "./pages/client/CounselorFeedback";
-// import FinalSessionSet from "./pages/FinalSessionSet";
-// import CounselorArticles from "./pages/counselor/CounselorArticles";
-// import BookingSession from "./pages/client/BookSession";
+import CounselorProfile from "./pages/counselor/CounelorDetail";
+import CounselorAvailability from "./pages/counselor/CounselorAvailability";
+import CounselorSchedule from "./pages/counselor/CounselorSchedule";
+import CounselorFeedback from "./pages/client/CounselorFeedback";
+import FinalSessionSet from "./pages/FinalSessionSet";
+import CounselorArticles from "./pages/counselor/CounselorArticles";
+import BookingSession from "./pages/client/BookSession";
 import UserRegister from "./pages/auth/signup";
 import ResetPassword from "./pages/auth/ResetPassword";
 import ResetSuccess from "./pages/auth/ResetPasswordSuccess";
 import ResetPasswordForm from "./pages/auth/ResetPasswordForm";
-// import Calendar from "./pages/counselor/Calendar";
-// import Booking from "./pages/client/Booking";
-
 
 const queryClient = new QueryClient();
 
@@ -54,23 +51,23 @@ const App = () => (
             <Route path="/reset-success" element={<ResetSuccess />} />
             <Route path="/reset-form" element={<ResetPasswordForm />} />
             <Route path="/verify-email" element={<EmailVerification />} />{" "}
-            {/* <Route path="/client-profile" element={<ClientCompleteProfile />} /> */}
-            {/* <Route
+            {/* <Route path="/client-profile" element={<ClientCompleteProfile />} />  */}
+            <Route
               path="/client-profile-next"
               element={<ClientProfileNext />}
             />
-            <Route path="/client-dashboard" element={<ClientDashboard />} />
-            <Route path="/counselor-posts" element={<CounselorPosts />} /> */}
+            {/* <Route path="/client-dashboard" element={<ClientDashboard />} /> */}
+            <Route path="/counselor-posts" element={<CounselorPosts />} />
             <Route
               path="/counselor-dashboard"
               element={<CounselorDashboard />}
             />
-            {/* <Route path="/counselor-profile" element={<CounselorProfile />} /> */}
-            {/* <Route path="/availability" element={<CounselorAvailability />} /> */}
-            {/* <Route path="/counselor-schedule" element={<CounselorSchedule />} /> */}
+            {/* <Route path="/counselor-profile" element={<CounselorProfile />} />  */}
+            {/* <Route path="/availability" element={<CounselorAvailability />} />  */}
+            {/* <Route path="/counselor-schedule" element={<CounselorSchedule />} />  */}
             {/* <Route path="/counselor-feedback" element={<CounselorFeedback />} /> */}
-            {/* <Route path="/final-session-set" element={<FinalSessionSet />} /> */}
-            {/* <Route path="/counselor-articles" element={<CounselorArticles />} /> */}
+            {/* <Route path="/final-session-set" element={<FinalSessionSet />} />  */}
+            <Route path="/counselor-articles" element={<CounselorArticles />} />
             {/* <Route path="/book-session" element={<BookingSession />} /> */}
             <Route path="*" element={<NotFound />} />
           </Routes>
